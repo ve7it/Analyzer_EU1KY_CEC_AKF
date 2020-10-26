a7d9bec417de1b7f799fb3b58b0df14475dc8080
Lawrence Glaister VE7IT <ve7it@shaw.ca>
2020-10-25 18:17:11 -0700

    update bin and hex

--- Src/Inc/build_timestamp.h
+++ Src/Inc/build_timestamp.h
@@ -1,7 +1,8 @@
 #ifndef BUILD_TIMESTAMP 
-#define HGREV 39b9eebf
-#define BUILD_TIMESTAMP_US "2020-10-24 09:38 UT"
-#define BUILD_TIMESTAMP_D "24-10-2020 09:38 UT"
+#warning GIT failed. Repository not found. Firmware revision will not be generated. 
+#define HGREV N/A 
+#define BUILD_TIMESTAMP_US "-~-2-~-2 ~-2:~-2 UT"
+#define BUILD_TIMESTAMP_D "~-2-~-2- ~-2:~-2 UT"
 #define HGREVSTR(s) stringify_(s) 
 #define stringify_(s) #s 
 #endif 
