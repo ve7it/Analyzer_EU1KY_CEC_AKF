4fbfbac925dbd0c9a52a6d4c177e45e9ee054f16
Lawrence Glaister VE7IT <ve7it@shaw.ca>
2020-11-01 17:16:10 -0800

    minor repair S21

--- Src/Inc/build_timestamp.h
+++ Src/Inc/build_timestamp.h
@@ -1,8 +1,8 @@
 #ifndef BUILD_TIMESTAMP 
 #warning GIT failed. Using build time/date not repository time/date 
 #define HGREV N/A 
-#define BUILD_TIMESTAMP_US "31/10/2020 7:57 PM"
-#define BUILD_TIMESTAMP_D "31/10/2020 7:57 PM"
+#define BUILD_TIMESTAMP_US "01/11/2020 5:09 PM"
+#define BUILD_TIMESTAMP_D "01/11/2020 5:09 PM"
 #define HGREVSTR(s) stringify_(s) 
 #define stringify_(s) #s 
 #endif 
